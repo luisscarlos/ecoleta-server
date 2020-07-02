de0c991a715ac118ca8ad7ce8cd9266252da4073
luisscarlos <luisscarlostec@gmail.com>
2020-07-02 04:48:21 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
+
+  response.json([
+    'Luis',
+    'Carlos',
+    'Santos'
+  ]);
 });
 
 app.listen(3333);
