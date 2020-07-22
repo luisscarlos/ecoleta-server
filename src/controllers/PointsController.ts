6d41299095568ecf96e6d78d3f24afa59b4cb972
luisscarlos <luisscarlostec@gmail.com>
2020-07-22 04:10:41 -0300

    Back-end finalizado

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
@@ -12,10 +30,12 @@ class PointsController {
     }
 
     // Retorna todos itens relacionados a esse ponto do parâmetro.
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
 
-    // 01:45
-
-    return response.json(point);
+    return response.json({ point, items });
   }
 
   async create(request: Request, response:Response) {
@@ -36,7 +56,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
@@ -60,6 +80,9 @@ class PointsController {
   
     // Inserção na tabela de relacionamento NxN 'point_items'
     await trx('point_items').insert(pointItems);
+
+    // De fato realiza a inserção na base de dados após validar as transaction
+    await trx.commit();
   
     return response.json({ 
       id:point_id,
